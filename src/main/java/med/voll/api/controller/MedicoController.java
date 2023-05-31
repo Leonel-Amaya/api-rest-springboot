100706a458ac586cb6b1299865fa4675229adb26
Leonel Amaya <omar.mejia2099@gmail.com>
2023-05-30 20:06:35 -0500

    Usango Get para traer los datos. Implementando paginación para ordenar los datos

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,14 +1,17 @@
 package med.voll.api.controller;
 
 import jakarta.validation.Valid;
+import med.voll.api.medico.DatoListadoMedico;
 import med.voll.api.medico.DatosRegistroMedicos;
 import med.voll.api.medico.Medico;
 import med.voll.api.medico.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/medicos")//Mapea endpoint
@@ -21,4 +24,16 @@ public class MedicoController {
     public void registrarMedico(@RequestBody @Valid DatosRegistroMedicos datosRegistroMedicos) {
         medicoRepository.save(new Medico(datosRegistroMedicos));
     }
+
+    //@GetMapping
+    //public List<DatoListadoMedico> listadoMedicos() {
+    //    return medicoRepository.findAll().stream().map(DatoListadoMedico::new).toList();
+    //}
+    //Esta porción del código trae consigo los datos seleccionados pero no de modo de paginación
+
+    @GetMapping
+    public Page<DatoListadoMedico> listadoMedicos(@PageableDefault(size = 2) Pageable paginacion) {
+        return medicoRepository.findAll(paginacion).map(DatoListadoMedico::new);
+    }
+    //Trae consigo los datos seleccionados en DatosListadoMedico pero formateados en páginas
 }
---
+++ src/main/java/med/voll/api/medico/DatoListadoMedico.java
@@ -0,0 +1,7 @@
+package med.voll.api.medico;
+
+public record DatoListadoMedico(String nombre, String especialidad, String documento, String email) {
+    public DatoListadoMedico(Medico medico) {
+        this(medico.getNombre(), medico.getEspecialidad().toString(), medico.getDocumento(), medico.getEmail());
+    }
+}
