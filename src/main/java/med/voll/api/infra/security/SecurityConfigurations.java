efca700c90ecec45b7de1fedb25b36afface479f
Leonel Amaya <omar.mejia2099@gmail.com>
2023-06-09 12:36:00 -0500

    Corrigiendo estructura código que lanzaba 403 Forbiden aún cuando se enviaba el token

--- src/main/java/med/voll/api/infra/security/SecurityConfigurations.java
+++ src/main/java/med/voll/api/infra/security/SecurityConfigurations.java
@@ -1,5 +1,6 @@
 package med.voll.api.infra.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -11,11 +12,20 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 //Las configuraciones son lo primero en lo que se fija Spring. Lo lee y continua la ejecución con ellas
 @Configuration
 @EnableWebSecurity
 public class SecurityConfigurations {
+    /*
+    Instanciamso un security filter para poder agregarlo como filtro en nuestr filterchain.
+    Al haber agregado que dejara libre la ruta /login éste bloquea el paso por nuestro securityfilter
+    Así que debemos agregarlo al método para que sea pasado antes de los filtros propios de spring
+     */
+    @Autowired
+    private SecurityFilter securityFilter;
+
     /*@Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity.csrf(csrf->csrf.disable())
@@ -35,8 +45,10 @@ public class SecurityConfigurations {
 //        return httpSecurity.csrf().disable().build();
         httpSecurity.csrf(csrf->csrf.disable())
                 .sessionManagement((sess-> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS)))//Le indicamos a spring el tipo de sesión
-                .authorizeHttpRequests((request -> request.requestMatchers(HttpMethod.POST,"/login").permitAll()//Los request de tipo post que vayan a login son permitidos, los demás no
-                        .anyRequest().authenticated()));
+                .authorizeHttpRequests((request -> request.requestMatchers(HttpMethod.POST,"/login")
+                        .permitAll()//Los request de tipo post que vayan a login son permitidos, los demás no
+                        .anyRequest().authenticated()))
+                .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class);
         return httpSecurity.build();
     }
 
--- src/main/java/med/voll/api/infra/security/SecurityFilter.java
+++ src/main/java/med/voll/api/infra/security/SecurityFilter.java
@@ -4,7 +4,10 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import med.voll.api.domain.usuarios.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -13,19 +16,31 @@ import java.io.IOException;
 @Component
 public class SecurityFilter extends OncePerRequestFilter {
     @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @Autowired
     private TokenService tokenService;
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        System.out.println("El filtro está siendo llamado");
-        var token = request.getHeader("Authorization");
-//        if (token == null || token == "") {
-////            throw new RuntimeException("El token enviado no es válido");
+        var authHeader = request.getHeader("Authorization");
+//        if (authHeader == null || authHeader == "") {
+////            throw new RuntimeException("El authHeader enviado no es válido");
 ////        }
-        //Revertimos el proceso y mejor se valida si el token no es nulo
-        if (token != null) {
-            token = token.replace("Bearer ", "");
-            System.out.println(token);
-            System.out.println(tokenService.getSubject(token));//Este usuario tiene sesion?
+        // Revertimos el proceso y mejor se valida si el authHeader no es nulo
+        if (authHeader != null) {
+            var token = authHeader.replace("Bearer ", "");
+            var nombreUsuario = tokenService.getSubject(token); // Extraemos el nombre de usuario
+            if (nombreUsuario != null) {
+                // Token válido
+                var usuario = usuarioRepository.findByLogin(nombreUsuario);// Encontramos al usuario
+                var authentication = new UsernamePasswordAuthenticationToken(usuario, null,
+                        usuario.getAuthorities()); // Forzamos un inicio de sesión
+                SecurityContextHolder.getContext().setAuthentication(authentication); // Setea manualmente en el securitycontext
+                // de spring esa autenticacion
+                // de modo que para los demás request el usuario ya va a estar autenticado
+
+            }
         }
         filterChain.doFilter(request, response);
     }
