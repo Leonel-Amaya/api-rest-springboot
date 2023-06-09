6d5b3f24eeccb6f29d396ed8608ad65d2ea94d90
Leonel Amaya <omar.mejia2099@gmail.com>
2023-06-09 11:24:19 -0500

    Permitiendo acceso a la ruta /login. Reacomodando la validación de token nulo

--- src/main/java/med/voll/api/infra/security/SecurityConfigurations.java
+++ src/main/java/med/voll/api/infra/security/SecurityConfigurations.java
@@ -2,6 +2,7 @@ package med.voll.api.infra.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -15,11 +16,28 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfigurations {
-    @Bean
+    /*@Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity.csrf(csrf->csrf.disable())
                 .sessionManagement((sess-> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS)))
                 .build();
+    }*/
+
+    /*
+    El método establece una autenticación stateless.
+    Permite la entrada a la ruta /login a todos los usuarios
+    Pero pide autenticación a cualquiera otra ruta
+     */
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+//        httpSecurity.sessionManagement(session -> session
+//                .sessionCreationPolicy( SessionCreationPolicy.STATELESS ));
+//        return httpSecurity.csrf().disable().build();
+        httpSecurity.csrf(csrf->csrf.disable())
+                .sessionManagement((sess-> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS)))//Le indicamos a spring el tipo de sesión
+                .authorizeHttpRequests((request -> request.requestMatchers(HttpMethod.POST,"/login").permitAll()//Los request de tipo post que vayan a login son permitidos, los demás no
+                        .anyRequest().authenticated()));
+        return httpSecurity.build();
     }
 
     /*
--- src/main/java/med/voll/api/infra/security/SecurityFilter.java
+++ src/main/java/med/voll/api/infra/security/SecurityFilter.java
@@ -4,6 +4,7 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -11,15 +12,21 @@ import java.io.IOException;
 
 @Component
 public class SecurityFilter extends OncePerRequestFilter {
+    @Autowired
+    private TokenService tokenService;
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         System.out.println("El filtro está siendo llamado");
         var token = request.getHeader("Authorization");
-        if (token == null || token == "") {
-            throw new RuntimeException("El token enviado no es válido");
+//        if (token == null || token == "") {
+////            throw new RuntimeException("El token enviado no es válido");
+////        }
+        //Revertimos el proceso y mejor se valida si el token no es nulo
+        if (token != null) {
+            token = token.replace("Bearer ", "");
+            System.out.println(token);
+            System.out.println(tokenService.getSubject(token));//Este usuario tiene sesion?
         }
-        token = token.replace("Bearer ", "");
-        System.out.println(token);
         filterChain.doFilter(request, response);
     }
 }
